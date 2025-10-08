80711ca87133d07c495aa0ac60d06c238a16e5d6
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-10-09 03:52:56 +0600

    add a search bar

--- src/Components/Apps/Apps.jsx
+++ src/Components/Apps/Apps.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLoaderData } from "react-router";
 import OurAllAppsCards from "../OurAllAppsCards/OurAllAppsCards";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const Apps = () => {
   const ourAllAppsData = useLoaderData();
@@ -12,6 +13,19 @@ const Apps = () => {
         Explore All Apps on the Market developed by us. We code for Millions
       </p>
 
+      <div className="flex justify-between">
+        <h2 className="text-2xl font-bold mt-7">
+          ({ourAllAppsData.length}) Apps Found
+        </h2>
+        <div className="flex items-center border border-gray-300 rounded-lg px-3 py-2 bg-white shadow-sm mt-4">
+          <AiOutlineSearch size={20} className="text-gray-400" />
+          <input
+            type="text"
+            placeholder="Search Apps"
+            className="ml-2 w-full outline-none text-gray-700 placeholder-gray-400"
+          />
+        </div>
+      </div>
       {/* parent div */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 mt-7">
         {ourAllAppsData.map((ourAllApp) => (
